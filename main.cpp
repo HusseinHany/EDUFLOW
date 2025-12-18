6e4d14287c4dd44eb76d6b03deeaae0dbd9cdd9c
Hussein Hany <132355394+HusseinHany@users.noreply.github.com>
2025-12-18 11:18:40 +0200

    Added main.cpp to the repository as outlined by description.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * FILE : main.cpp
+ * DESCRIPTION : This file's purpose as much as to implement the main interface to the system. Complete description and implementation
+ for functionality and requirements are outlined in the particular repository update notes.
+ */
+#include<iostream>
+using namespace std;
+// Function prototypes or other includes based required instruction of Prompt FILE 
+
+int main() {
+    cout<<"Success Placeholder please customize it / "<<endline;
+}
